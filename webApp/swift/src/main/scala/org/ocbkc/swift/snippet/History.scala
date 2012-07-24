c8ea9a750ce6a61a45e3b6f7244a1c6e42422f8d
Chide <cg@xs4all.nl>
2012-07-24 20:52:39 +0200

    preparing view historic version

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/History.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/History.scala
@@ -31,9 +31,9 @@ class History
    {  println("historyTableRows")
       if( const == null ) println("   bug: const == null")
       const.getHistory.flatMap( revcom => bind( "top", chooseTemplate("top", "row", ns),            
+         // TODO "view"               -> SHtml.link("constitution?constid=" + const.id + "&commitid=" + revcom.name(), () => Unit, Text("view")), // <&y2012.07.12.20:59:23& seems to be some additional garbage returned by ObjectId.toString, look up in jgit comunity how to get rid of it.>
          "restore"            -> SHtml.link("restore?constid=" + const.id + "&commitid=" + revcom.name(), () => Unit, Text("restore")), // <&y2012.07.12.20:59:23& seems to be some additional garbage returned by ObjectId.toString, look up in jgit comunity how to get rid of it.>
-         "checkbox"           -> SHtml.checkbox(false, processCheckbox(_, revcom)),//Text("TODO"), // <&y2012.07.12.21:12:42& only found SHtml.checkbox, which forces you to define all checkboxes at once, while I want to create1 checkbox per tag-substitution. For Koen?>
-         
+         "checkbox"           -> SHtml.checkbox(false, processCheckbox(_, revcom)),
          "publishDescription" -> Text(revcom.getFullMessage()),
          "date"               -> Text(revcom.getCommitTime().toString),
          "author"             -> Text(revcom.getAuthorIdent().toString)
