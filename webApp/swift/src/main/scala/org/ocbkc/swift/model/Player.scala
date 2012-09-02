1754bfd2f5c5337a03ef1c1094b4a3789a42836d
Chide <cg@xs4all.nl>
2012-09-02 20:40:02 +0200

    refactor constiSelectionProcedures -> Mapper Not clear how to do this, pausing this branch for a while...

--- webApp/swift/src/main/scala/org/ocbkc/swift/model/Player.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/model/Player.scala
@@ -44,12 +44,7 @@ class Player extends MegaProtoUser[Player] {
    {  firstName.get + " " + lastName.get + " (id: " + userIdAsString + ")"
    }
 
-/*
-   val constiSelectionProcedure = OneToStartWith // embodies which procedure is followed to let people study the constitutions, e.g.: allow them to choose only one constitution and study it the first time they play, or allow any constitution to be consulted at any moment etc. etc.
-*/
-
-   object ridiculousTestBoolean extends MappedBoolean(this) // <&y2012.08.04.19:43:17& set this to false after first session has been completed (or other conditions?)> 
-   object ridiculousTestString extends MappedString(this, 100) // <&y2012.08.04.19:43:17& set this to false after first session has been completed (or other conditions?)>    
+WIW doens't work - isn't instance   object constiSelectionProcedure extends MappedLongForeignKey(this, ConstiSelectionProcedure) // embodies which procedure is followed to let people study the constitutions, e.g.: allow them to choose only one constitution and study it the first time they play, or allow any constitution to be consulted at any moment etc. etc.
 
    object firstChosenConstitution extends MappedLongForeignKey(this, ConstitutionMetaMapperObj) // <&y2012.08.03.10:20:25& perhaps in future refactor, or supplement, with more generic, row of chosen constitutions>
 
