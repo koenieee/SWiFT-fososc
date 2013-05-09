a2d9cef9c2eae856be54b756a95d808da108703a
Koen <koenvdkruk@gmail.com>
2013-05-09 13:01:17 +0200

    Html Blank Out in study consti

--- webApp/swift/src/main/scala/org/ocbkc/swift/global/Global.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/global/Global.scala
@@ -129,7 +129,7 @@ object Types
 object TestSettings
 {  val AUTOLOGIN                       = false
    val AUTOTRANSLATION                 = false // true
-   val CREATETESTUSERBASE              = false // true
+   val CREATETESTUSERBASE              = false// true
    /* <&y2012.09.29.19:44:55& TODO: if constitutions DO exist, don't create new constitutions. Or perhaps better: erase them but not before prompting the developer> */
    val CREATEDUMMYCONSTITUTIONS        = false // true // creates a number of constitutions with several updates and releases, but also some users.
    val SIMULATEPLAYINGWITHJARA         = false // Simulate playing with Jara during Boot. After boot normal playing (by real persons) can be continued from there.
