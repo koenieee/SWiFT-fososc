07fcd1b606d5657db6e5f823f0ad654d1fa5097b
Chide <cg@xs4all.nl>
2013-06-03 20:54:57 +0200

    develop.questionnare: in progress

--- webApp/swift/src/main/scala/org/ocbkc/swift/questionnaire/Questionnaire.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/questionnaire/Questionnaire.scala
@@ -3,22 +3,20 @@
   */
 
 package org.ocbkc.questionnaire
-{  class Questionnaire extends List[Question]
-   {
+{  class Questionnaire
+   {  var questions:List[Question] = Nil
    }
 
    trait Question
+   {  var respondentsAnswer:Option[Answer] = None
+   }
 
    trait Answer
-   {  var Question = 
+   {  var Question:Question
    }
 
-   abstract case class MC_Option(val code, val text)
-   {  
-   }
-
-   class FreeTextAnswer extends Answer
-   {  
+   abstract case class MC_Option(val code:String, val text:String)
+   {
    }
 
    trait ClosedQuestion extends Question
@@ -32,16 +30,21 @@ package org.ocbkc.questionnaire
       var maximumNumberOfAnswers:Int = 1
 
       case class MC_Option // an innner class MC_Option forces all code in this class which uses MC_Option to only contain options of THIS MultipleChoiceQuestion instance...
-      class MC_Answer extends List[MC_Option] with Answer
-      var options:List[MC_Option] = Nil
+      class MC_Answer extends Answer
+      {  var options:List[MC_Option] = Nil
+      }
 
-      private var _correctAnswer:MC_Answer
+      var options:List[MC_Option] = Nil // not to confuse with Scala's Option. This is a multiple choice option.
 
+      private var _correctAnswer:MC_Answer = new MC_Answer
+
+      /** @return None (correctAnswer is unknown), or Some(non-empty list of MC_Options) as answer.
+        */
       override def correctAnswer:Option[MC_Answer] =
-      {  _correctAnswer match
-         {  case Nil          => None
-            case Some(lmco)   => Some(lmco)
-         }
+      {  if(_correctAnswer.options.isEmpty)
+            None
+         else
+            Some(_correctAnswer)
       }
 
       def correctAnswer_=(mca:MC_Answer) =
@@ -49,7 +52,35 @@ package org.ocbkc.questionnaire
       }
    }
 
-   class FreeTextClosedQuestion extends ClosedQuestion
-   {  
+   case class FreeTextAnswer extends Answer
+   {  val text:String
+   }
+
+   trait FreeTextQuestion extends Question
+   {  private var _correctAnswer:FreeTextAnswer = new FreeTextAnswer
+
+      /** @return None (correctAnswer is unknown), or Some(non-empty list of MC_Options) as answer.
+        */
+      override def correctAnswer:Option[MC_Answer] =
+      {  if(_correctAnswer.options.isEmpty)
+            None
+         else
+            Some(_correctAnswer)
+      }
+
+      def correctAnswer_=(mca:MC_Answer) =
+      {  _correctAnswer = mca
+      }
+
+   }
+
+   class FreeTextClosedQuestion extends FreeTextQuestion with ClosedQuestion
+   {
+   }
+
+   /** FreeTextAnswer applies to both FreeTextClosedQuestion and FreeTextOpenQuestion
+     */
+   case class FreeTextAnswer(question:FreeTextQuestion) extends Answer
+   {
    }
 }
