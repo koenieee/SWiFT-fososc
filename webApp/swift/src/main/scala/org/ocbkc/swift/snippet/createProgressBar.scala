8f9f9d27914397daa324a089d7da0c16f84d32e4
koen-github <koen@koen-mint.(none)>
2013-08-16 00:54:23 +0200

    more elegant solution html in lift

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/createProgressBar.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/createProgressBar.scala
@@ -3,6 +3,7 @@ package org.ocbkc.swift.snippet
 import _root_.scala.xml._
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.http._
+import js.JsCmds.SetHtml
 import _root_.net.liftweb.common._
 import _root_.java.util.Date
 import org.ocbkc.swift.lib._
@@ -29,14 +30,15 @@ class createProgress
    
               println("   latestRoundFluencySession = " + lrfs)
                lrfs match
-               {  case NotInFluencySession => <center><a href="startSession.html">Start</a> <b>-></b> Translation <b>-></b> Bridge Construction <b>-></b> Question Attack <b>-></b> Algorithmic Defence <b>-></b> Algorithmic 2 <b>-></b> End session</center>
-                  case RoundTranslation    => <center><a href="startSession.html">Start</a> <b>-></b><a href="translationRound.html" > Translation </a><b>-></b> Bridge Construction <b>-></b> Question Attack <b>-></b> Algorithmic Defence <b>-></b> Algorithmic 2 <b>-></b> End session</center>
+               {  
+				   case NotInFluencySession => chooseTemplate("pross", "1", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!) //Templates("templates-hidden" :: "progressbar2" :: Nil).map{ns => <x>{ns}</x>} toSeq
+                  case RoundTranslation    => chooseTemplate("pross", "2", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
 
-                  case RoundBridgeConstruction => <center><a href="startSession.html">Start</a> <b>-></b><a href="translationRound.html"> Translation </a><b>-></b> <a href="bridgeconstruction.html">Bridge Construction</a> <b>-></b> Question Attack <b>-></b> Algorithmic Defence <b>-></b> Algorithmic 2 <b>-></b> End session</center>
-                  case RoundQuestionAttack => <center><a href="startSession.html">Start</a> <b>-></b><a href="translationRound.html"> Translation </a><b>-></b> <a href="bridgeconstruction.html">Bridge Construction</a> <b>-></b> <a href="questionAttackRound.html">Question Attack</a> <b>-></b> Algorithmic Defence <b>-></b> Algorithmic 2 <b>-></b> End session</center>
-                  case RoundAlgorithmicDefenceStage1 => <center><a href="startSession.html">Start</a> <b>-></b><a href="translationRound.html"> Translation </a><b>-></b> <a href="bridgeconstruction.html">Bridge Construction</a> <b>-></b> <a href="questionAttackRound.html">Question Attack</a> <b>-></b> <a href="algorithmicDefenceRound.html">Algorithmic Defence</a> <b>-></b> Algorithmic 2 <b>-></b> End session</center>
-                 case RoundAlgorithmicDefenceStage2 =>  <center><a href="startSession.html">Start</a> <b>-></b><a href="translationRound.html"> Translation </a><b>-></b> <a href="bridgeconstruction.html">Bridge Construction</a> <b>-></b> <a href="questionAttackRound.html">Question Attack</a> <b>-></b> <a href="algorithmicDefenceRound.html">Algorithmic Defence</a> <b>-></b> <a href="algorithmicDefenceRoundStage2.html">Algorithmic 2 </a><b>-></b> End session</center>
-                  case _                   => <center><a href="startSession.html">Start</a> <b>-></b><a href="translationRound.html"> Translation </a><b>-></b> <a href="bridgeconstruction.html">Bridge Construction</a> <b>-></b> <a href="questionAttackRound.html">Question Attack</a> <b>-></b> <a href="algorithmicDefenceRound.html">Algorithmic Defence</a> <b>-></b> <a href="algorithmicDefenceRoundStage2.html">Algorithmic 2</a> <b>-></b> End session</center>
+                  case RoundBridgeConstruction => chooseTemplate("pross", "3", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
+                  case RoundQuestionAttack => chooseTemplate("pross", "4", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
+                  case RoundAlgorithmicDefenceStage1 => chooseTemplate("pross", "5", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
+                 case RoundAlgorithmicDefenceStage2 =>  chooseTemplate("pross", "6", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
+                  case _                   => chooseTemplate("pross", "7", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
                } 
             /*   
    x match
