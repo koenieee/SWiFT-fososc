686a6d21b9da29ddffa0cd2ed26184f73708aac3
Koen van der Kruk <koenvdkruk@gmail.com>
2013-08-23 17:41:44 +0200

    after about 100 different version, still nothing

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/createProgressBar.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/createProgressBar.scala
@@ -22,17 +22,50 @@ class createProgress
 {
 	//Please implemt RoundAlgorithmicDefenceStage2
 
-  def text: NodeSeq = {
+def blaat= {
+	
+             
+         val names = List((".start [href]" #> "startSession.html"),(".translation [href]" #> "translationRound.html"),(".bridge [href]" #>  "bridgeconstruction.html"),(".question [href]" #> "questionAttackRound.html"),(".defence1 [href]" #> "algorithmicDefenceRound.html"),(".defence2 [href]" #> "algorithmicDefenceRoundStage2.html"))
+       
+  
+  ".urls *" #> names
+              
+}
+
+  def text:NodeSeq= {
 	  println("createprogressbar is called");
    val  x = S.attr("w_page") openOr "geen param :("
    val lrfs = sesCoord.is.latestRoundFluencySession
    //println(sesCoord.is.latest);
    
               println("   latestRoundFluencySession = " + lrfs)
+              TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!
+              //startSession.html: start
+              //translationRound.html: translation
+              //bridgeconstruction.html: bridge
+              //href="questionAttackRound.html:"question
+              //href="algorithmicDefenceRound.html": defence1
+              //href="algorithmicDefenceRoundStage2.html": defence2
+              //val urls = List(AttrBindParam("start","startSession.html","href"),AttrBindParam("translation","translationRound.html","href"),AttrBindParam("bridge","bridgeconstruction.html","href"),AttrBindParam("question","questionAttackRound.html","href"),AttrBindParam("defence1","algorithmicDefenceRound.html","href"),AttrBindParam("defence2","algorithmicDefenceRoundStage2.html","href"))
+             //List.flatten(urls.slice(0,2))
+              
+         
+               
+  
+              /*            
+              bind("b",xhtml, 
+              AttrBindParam("start","startSession.html","href"),
+              AttrBindParam("translation","translationRound.html","href"),
+              AttrBindParam("bridge","bridgeconstruction.html","href"),
+              AttrBindParam("question","questionAttackRound.html","href"),
+              AttrBindParam("defence1","algorithmicDefenceRound.html","href"),
+              AttrBindParam("defence2","algorithmicDefenceRoundStage2.html","href")
+              )
+  
                lrfs match
                {  
-				   case NotInFluencySession => chooseTemplate("pross", "1", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!) //Templates("templates-hidden" :: "progressbar2" :: Nil).map{ns => <x>{ns}</x>} toSeq
-                  case RoundTranslation    => chooseTemplate("pross", "2", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
+				   case NotInFluencySession => translationRound.html
+                  case RoundTranslation    => chooseTemplate("round", "translation", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
 
                   case RoundBridgeConstruction => chooseTemplate("pross", "3", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
                   case RoundQuestionAttack => chooseTemplate("pross", "4", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
@@ -40,6 +73,9 @@ class createProgress
                  case RoundAlgorithmicDefenceStage2 =>  chooseTemplate("pross", "6", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
                   case _                   => chooseTemplate("pross", "7", TemplateFinder.findAnyTemplate(List("templates-hidden", "progressbar")).open_!)
                } 
+               
+               */
+               
             /*   
    x match
    {
