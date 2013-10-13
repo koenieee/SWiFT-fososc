0687904e3084ffd21e6439e420cffbe76301ba1f
daizoo <daizoo@daizoo-1025C.(none)>
2013-10-13 15:37:57 +0200

    ad

---
+++ webApp/swift/src/main/scala/org/ocbkc/swift/parser/EfeLang.scala
@@ -0,0 +1,25 @@
+/*
+Part of SWiFT game (c) Chide Groenouwe
+
+
+
+### CONVENTIONS
+None so far
+
+*/
+
+package org.ocbkc.swift.parser
+{
+import scala.collection.JavaConversions._
+import scala.util.parsing.combinator._
+import System._
+import java.io._
+import org.ocbkc.swift.logilang._
+// import org.ocbkc.swift.logilang.FOLutils // <&y2012.04.04.20:10:13& remove this line when debugging done>
+
+class Efe extends ...
+{  def efeDocument =
+   "B("
+}
+
+
