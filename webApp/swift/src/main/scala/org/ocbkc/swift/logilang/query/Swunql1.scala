f444a722c41a44cbba8c46746902c09e22628bb3
Chide <c.groenouwe@ati-a.nl>
2013-11-25 22:17:15 +0100

    add

---
+++ webApp/swift/src/main/scala/org/ocbkc/swift/logilang/query/Swunql1.scala
@@ -0,0 +1,14 @@
+package org.ocbkc.swift.logilang.query.swunql
+{
+import System._
+import java.io._
+import org.ocbkc.swift.logilang._
+import org.ocbkc.swift.logilang.query._
+import org.ocbkc.swift.parser.CLIwithFileInput
+import net.liftweb.json._
+
+case class SwunqlSent
+{  
+}
+
+
