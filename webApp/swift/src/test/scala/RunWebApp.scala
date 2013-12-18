0ffefc6b0b987f2d245efe14dd0e1f0ee0244e65
Mussie1 <gergykaas@gmail.com>
2013-12-18 19:44:32 +0100

    Study Nettify 5

--- webApp/swift/src/test/scala/RunWebApp.scala
+++ webApp/swift/src/test/scala/RunWebApp.scala
@@ -16,19 +16,19 @@ object RunWebApp extends Application
 
    server.addHandler(context)
 
-try 
-{  println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP")
-   server.start()
-      while (System.in.available() == 0) 
-      {   Thread.sleep(5000)
-       }
-        server.stop()
-	server.join()
-} catch {
-  case exc : Exception => 
-    {  exc.printStackTrace()
-       System.exit(100)
-  }
- }
+   try 
+   {  println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP")
+      server.start()
+         while (System.in.available() == 0) 
+         {  Thread.sleep(5000)
+         }
+          server.stop()
+	  server.join()
+   } catch {
+     case exc : Exception => 
+       {  exc.printStackTrace()
+          System.exit(100)
+     }
+  } 
 }
 
