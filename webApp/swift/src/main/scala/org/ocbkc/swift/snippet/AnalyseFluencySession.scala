e81c0009e746c5da8ae2d3833e3f3760de5d6306
Chide <cg@xs4all.nl>
2014-04-20 19:00:47 +0200

    develop.fluency_ses_ana_tools in progress

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/AnalyseFluencySession.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/AnalyseFluencySession.scala
@@ -24,32 +24,6 @@ import _root_.net.liftweb.widgets.tablesorter.TableSorter
 class AnalyseFluencySession
 {  val sesCoordLR = SesCoord.is // extract session coordinator object from session variable
       
-   class URLparamExtractionResult
-   case class PlayerParamMissing extends ParamExtra
-   case class WIW perhaps even better to do this for each (potential) parameter in a list
-   or perhaps it is better to just move this (the S.param) to the render function...
-
-   val playerOption:Option[Player] = S.param("player_id") match
-   {  case Full(player_id)  =>
-      {  val msgStart = "   Player with id " + player_id
-
-         Player.find(player_id) match
-         {  case Full(player) => {  log(msgStart + " found!")
-                                    Some(player)
-                                 } // do nothing, player exists.
-            case _            =>
-            {  log("   " + msgStart + " not found... Me not happy. But ain't a bug I guess.")
-               PlayerParamMissing
-            }
-         }
-      }
-      case _ =>
-      {  log("Parameter player_id missing in URL.")
-         None
-      }
-   }
-
-
    def sessionTableRows(ns:NodeSeq, player:Player):NodeSeq =
    {  log("sessionTableRows called")
       
@@ -81,9 +55,8 @@ class AnalyseFluencySession
    }
 
    def render(ns: NodeSeq): NodeSeq =
-   {  playerOption match
-      {  case Some(player) => // bind player related fields
-         {  val playerBindResult =
+   {  def renderWhenPlayerExists(p:Player) =
+      {           {  val playerBindResult =
                bind( "top", ns,
                      "player"       -> Text(player.id.asString)
                )
@@ -96,7 +69,7 @@ class AnalyseFluencySession
                   case None => None
                }
 
-            constiOption match
+            constiOption match // is there a first chosen consti for this player?
             {  case Some(consti) => // bind consti related fields
                {  bind( "top", playerBindResult,
                      "constName"    -> Text(consti.constiId.toString),
@@ -114,13 +87,34 @@ class AnalyseFluencySession
                }
             }
          }
-         case None =>
-         {  log("[SHOULDDO] simply do not show any consti related info in this case, but notify the constigame player that the player did not yet chose a consti")
+      }
+
+      def renderWhenNoPlayerExists =
+      {  log("[SHOULDDO] simply do not show any consti related info in this case, but notify the constigame player that the player did not yet chose a consti")
             bind( "top", ns,
                   "constName"    -> Text("No constitution chosen by this player yet"),
                   "release"      -> Text("-"),
                   "sessionTable" -> NodeSeq.Empty 
             )
+      }
+
+      S.param("player_id") match
+      {  case Full(player_id)  =>
+         {  val msgStart = "   Player with id " + player_id
+
+            Player.find(player_id) match
+            {  case Full(player) => {  log(msgStart + " found!")
+                                       renderWhenPlayerExists(player)
+                                    } // do nothing, player exists.
+               case _            =>
+               {  log("   " + msgStart + " not found... Me not happy. But doesn't have to be a bug.")
+                  S.redirectTo("todoerror_no_player_not_found")
+               }
+            }
+         }
+         case _ =>
+         {  log("Parameter player_id missing in URL.")
+            S.redirectTo("index")
          }
       }
    }
