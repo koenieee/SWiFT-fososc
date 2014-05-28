51e1d831f3b5e5d996e1409a26b1e490da847f20
koen-github <koenvdkruk@gmail.com>
2014-05-28 23:42:41 +0200

    Code refactering, added sessionDetails, other updates

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/AnalyseFluencySessionsOfRelease.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/AnalyseFluencySessionsOfRelease.scala
@@ -1,91 +1,93 @@
-package org.ocbkc.swift 
+package org.ocbkc.swift
 {
-package snippet 
+package snippet
 {
 import org.ocbkc.swift.global.Logging._
 import _root_.scala.xml._
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.http._
 import _root_.net.liftweb.common._
-import _root_.java.util.Date
-import org.ocbkc.swift.lib._
 import org.ocbkc.swift.OCBKC._
 import org.ocbkc.swift.OCBKC.scoring._
 import org.ocbkc.swift.OCBKC.ConstitutionTypes._
-import org.ocbkc.swift.global._
-import org.ocbkc.swift.general.GUIdisplayHelpers._
 import Helpers._
-import System.err.println
-import org.ocbkc.swift.model._
-import org.eclipse.jgit.revwalk.RevCommit 
-import org.eclipse.jgit.lib.ObjectId
-import org.ocbkc.swift.global.LiftHelpers._
 import _root_.net.liftweb.widgets.tablesorter.TableSorter
 
 class AnalyseFluencySessionsOfRelease
-{  val sesCoordLR = SesCoord.is // extract session coordinator object from session variable
+{ val sesCoordLR = SesCoord.is // extract session coordinator object from session variable
 
-   def playerTableRows(ns:NodeSeq, release_id:VersionId):NodeSeq =
-   {  log("sessionTableRows called")
-      
-      TableSorter("#sessionOfReleaseTable")
-         
-      implicit val displayIfNone = "-"
+  def playerTableRows(ns:NodeSeq, release_id:VersionId):NodeSeq =
+  { log("sessionTableRows called")
 
-      // create headers
-      val header = 
-         bind(
-            "top", chooseTemplate("top", "row", ns),
-               "playerId"        -> <b>Player</b>,
-               "fluency"         -> <b>Fluency</b>,
-               "masteredChallenge"        -> <b>Master</b>,
-               "averageTranslationTime"   -> <b>Average Translation Time </b>,
-               "shortestTransTime"        -> <b> Shortest Translation Time </b>,
-               "sessionsPlayedB4accessToAllConstis"   -> <b> Total valid sessions played </b>
-         )
-   
-      // create data rows
-      header ++
-      Constitution.playersWithRelease(release_id).flatMap
-      {  p =>
-         {  val df = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm")
+    TableSorter("#sessionOfReleaseTable")
 
-            bind( "top", chooseTemplate("top", "row", ns),
-               "playerId"        -> { Text(p.swiftDisplayName) },
-               "fluency"         -> { Text("TODO") }, // PlayerScores.fluencyScoreSample(p)
-               "masteredChallenge"        -> { Text("TODO") },
-               "averageTranslationTime"   -> { Text(PlayerScores.averageDurationTranslation(p).averageDurationTranslation.get.toString) },
-               "shortestTransTime"        -> { Text("TODO -> see showduration highscores") },
-               "sessionsPlayedB4accessToAllConstis"   -> Text("TODO")
-            )
-         }
+    implicit val displayIfNone = "-"
+
+    // create headers
+    val header =
+    bind(
+    "top", chooseTemplate("top", "row", ns),
+    "playerId"                             -> <b>Player</b>,
+    "fluency"                              -> <b>Fluency</b>,
+    "masteredChallenge"                    -> <b>Master</b>,
+    "averageTranslationTime"               -> <b>Average Translation Time </b>,
+    "shortestTransTime"                    -> <b> Shortest Translation Time </b>,
+    "sessionsPlayedB4accessToAllConstis"   -> <b> Total valid sessions played </b>,
+    "sessionLink"                          -> <b>Session Link</b>
+    )
+
+    // create data rows
+    header ++
+    Constitution.playersWithRelease(release_id).flatMap
+    { p =>
+      { val df = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm")
+
+        bind( "top", chooseTemplate("top", "row", ns),
+        "playerId"                             -> { Text(p.swiftDisplayName) },
+        "fluency"                              -> { Text("Todo") }, // PlayerScores.fluencyScoreSample(p)
+        "masteredChallenge"                    -> { Text("TODO") },
+        "averageTranslationTime"               -> { Text(PlayerScores.averageDurationTranslation(p).averageDurationTranslation.get.toString) },
+        "shortestTransTime"                    -> { Text("TODO -> see showduration highscores") },
+        "sessionsPlayedB4accessToAllConstis"   -> Text("todo"),
+        "sessionLink"                          -> SHtml.link("analyseFluencySessionsPlayer.html?player_id="+p.id,()=>(),Text("Player Session"))
+        )
       }
-   }
+    }
+  }
 
-   def render(ns: NodeSeq): NodeSeq =
-   {  S.param("release_id") match
-      {  case Full(release_id) =>
-         {  val msgStart = "   Release with id " + release_id
+  def render(ns: NodeSeq): NodeSeq =
+  {
 
-            if( Constitution.releaseExists(release_id) )
-            {  log( msgStart + " found!")
-               bind( "top", ns,
-                  "sessionOfReleaseTable" -> playerTableRows(ns, release_id),
-               "constName" -> Text("TODO"),
-               "release" -> Text("release Name")
-               )
-            } else
-            {  log( "[POTENTIAL_BUG] " + msgStart + " not found... Me not happy. Perhaps the player used an old link to a release which has been deleted in the meanwhile.")
-               S.redirectTo("index")
-            }
-         }
+    (S.param("release_id"), S.param("consti_id")) match
+    { case (Full(release_id),Full(consti_id)) =>
+      { val msgStart = "   Release with id " + release_id
 
-         case _ =>
-         {  log(" Parameter release_id missing in URL.")
-            S.redirectTo("index")
-         }
+        if( Constitution.releaseExists(release_id) )
+        { log(msgStart + " found!")
+          val constiOption = Constitution.getById(consti_id.toInt)
+
+          log("ConstiOption" +constiOption)
+
+          bind( "top", ns,
+          "sessionOfReleaseTable"    -> playerTableRows(ns, release_id),
+          "constName"                -> Text(constiOption.get.constiId.toString),
+          "release"                  -> Text(release_id)
+          )
+        }
+        else
+        { log( "[POTENTIAL_BUG] " + msgStart + " not found... Me not happy. Perhaps the player used an old link to a release which has been deleted in the meanwhile.")
+
+          S.redirectTo("../index")
+        }
       }
-   }
+
+      case _ =>
+      { log(" Parameter release_id or consti_id missing in URL.")
+
+        S.redirectTo("../index")
+      }
+    }
+  }
 }
 
 }
--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/analyseFluencySessionsPlayer.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/analyseFluencySessionsPlayer.scala
@@ -1,7 +1,6 @@
 package org.ocbkc.swift.snippet
 
 import scala.xml.{Text, NodeSeq}
-import org.ocbkc.swift.OCBKC.ConstitutionTypes._
 import org.ocbkc.swift.global.Logging._
 import net.liftweb.widgets.tablesorter.TableSorter
 import net.liftweb.util.Helpers._
@@ -12,13 +11,11 @@ import net.liftweb.common.Full
 import org.ocbkc.swift.model._
 import net.liftweb.mapper.By
 
-
-
 class analyseFluencySessionsPlayer {
   val sesCoordLR = SesCoord.is // extract session coordinator object from session variable
 
   def sessionPlayerTable(ns:NodeSeq, playerID: Player):NodeSeq =
-  {  log("sessionPlayerTable called")
+  { log("sessionPlayerTable called")
 
     TableSorter("#analyseFluencySessionsPlayer")
 
@@ -26,76 +23,61 @@ class analyseFluencySessionsPlayer {
 
     // create headers
     val header =
-      bind(
-        "top", chooseTemplate("top", "row", ns),
-        "session"        -> <b>Session</b>,
-        "score"         -> <b>Score</b>,
-        "transTime"        -> <b>Translation Time</b>,
-        "answerCor"   -> <b>Answer Correct</b>,
-        "detailsLink"        -> <b>Details Link</b>
+    bind("top", chooseTemplate("top", "row", ns),
+      "session"       -> <b>Session</b>,
+      "score"         -> <b>Score</b>,
+      "transTime"     -> <b>Translation Time</b>,
+      "answerCor"     -> <b>Answer Correct</b>,
+      "detailsLink"   -> <b>Details Link</b>
+    )
 
-      )
-
-//header
-    // create data rows
     log("Fluency Score: "+ PlayerScores.fluencyScoreSample(playerID).toString())
-   header ++
-     (sesCoordLR.sessionsPlayedBy(playerID) zip PlayerScores.fluencyScoreSample(playerID)).flatMap
-     {
-       session =>
 
-         bind( "top", chooseTemplate("top", "row", ns),
-           "session"        -> { Text(session._1.id.toString) },
-           "score"         -> { Text(session._2.toDouble.toString) },
-           "transTime"        -> { Text(session._1.durationTranslation.get.toString) },
-           "answerCor"   -> { Text(session._1.answerPlayerCorrect.get match { case true => "Yes" case false => "No"}) },
-           "detailsLink"        -> { SHtml.link("analyseFluencySessionDetails.html?session_id="+session._1.id.toString,()=>(),Text("Details Link")) }
+    header ++
+    sesCoordLR.sessionsPlayedBy(playerID).flatMap
+    { session =>
 
-         )
-
-     }
-           /*
-      Constitution.playersWithRelease(release_id).flatMap
-      {  p =>
-      {  val df = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm")
-
-
-      }
-      }*/
-
+      bind( "top", chooseTemplate("top", "row", ns),
+      "session"        -> { Text(session.id.toString) },
+      "score"          -> { Text("TODO, FLuencyScore function by W") },
+      "transTime"      -> { Text(session.durationTranslation.get.toString) },
+      "answerCor"      -> { Text(session.answerPlayerCorrect.get match { case true => "Yes" case false => "No"}) },
+      "detailsLink"    -> { SHtml.link("analyseFluencySessionDetails.html?session_id="+session.id.toString,()=>(),Text("Details Link")) }
+      )
+    }
   }
 
   def render(ns: NodeSeq): NodeSeq =
-  {  S.param("player_id") match
-  {  case Full(player_id) =>
-    {  val msgStart = "Input player_ID: " + player_id
-      println(Player.find(By(Player.id, player_id.toLong)))
-      val player = Player.find(By(Player.id, player_id.toLong))
-      if( !player.isEmpty)
-      {  log( msgStart + " found!")
-        val constiOption:Option[Constitution] = Constitution.getById(player.get.firstChosenConstitution.is)
-        log(constiOption.toString)
-//I get sometimes a None.Get. But the player exists, why is constiOption empty, if there are sessions played..?
-        bind( "top", ns,
-          "sessionPlayerTable" -> sessionPlayerTable(ns, player.get),
-          "player" -> Text(player.get.firstName),
-        "constName" -> Text(constiOption.get.constiId.toString),
-        "release" -> Text(constiOption.get.currentVersionId)
+  { S.param("player_id") match
+    { case Full(player_id) =>
+      { val msgStart = "Input player_ID: " + player_id
+        //println(Player.find(By(Player.id, player_id.toLong)))
 
-        )
-      } else
-      {  log( "[POTENTIAL_BUG] " + msgStart + " not found.")
+        val player = Player.find(By(Player.id, player_id.toLong))
+
+        if( !player.isEmpty )
+        { log( msgStart + " found!")
+          val constiOption:Option[Constitution] = Constitution.getById(player.get.firstChosenConstitution.is)
+          log(constiOption.toString)
+          //I get sometimes a None.Get. But the player exists, why is constiOption empty, if there are sessions played..?
+
+          bind( "top", ns,
+            "sessionPlayerTable"  -> sessionPlayerTable(ns, player.get),
+            "player"              -> Text(player.get.firstName),
+            "constName"           -> Text(constiOption.get.constiId.toString),
+            "release"             -> Text(constiOption.get.currentVersionId)
+          )
+        }
+        else
+        { log("[POTENTIAL_BUG] " + msgStart + " not found.")
+          S.redirectTo("../index")
+        }
+      }
+
+      case _ =>
+      { log(" Parameter player_id missing in URL.")
         S.redirectTo("../index")
       }
     }
-
-    case _ =>
-    {  log(" Parameter player_id missing in URL.")
-      S.redirectTo("../index")
-    }
   }
-  }
-//TODO by KOEN
-
-
 }
