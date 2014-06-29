42e7a11adae70431b43ff90568de65de5107652c
koen-github <koenvdkruk@gmail.com>
2014-06-29 16:05:25 +0200

    updates in d3js

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/FluencyTimeSeries.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/FluencyTimeSeries.scala
@@ -3,7 +3,11 @@ package org.ocbkc.swift.snippet
 
 import _root_.scala.xml._
 import net.liftweb.util.Helpers._
-
+import net.liftweb.http.S
+import net.liftweb.common.Full
+import org.ocbkc.swift.model.{SessionInfoMetaMapperObj, Player}
+import net.liftweb.mapper.By
+import org.ocbkc.swift.global.Logging._
 /**
  * Created by koen on 28-6-14.
  */
@@ -11,15 +15,48 @@ class FluencyTimeSeries {
 
   def render(xhtml:NodeSeq):NodeSeq=
   {
-    bind("top",xhtml,
-        "user" -> Text("todo"),
-        "input" ->Text("todo"),
-        "graph" ->Text("todo")
+    S.param("player") match {
+      case Full(player_id) =>
+      {
+       // val player = Player.find(By(Player.id, player_id.toLong))
+        val sessionsByPlayer = SessionInfoMetaMapperObj.find(By(SessionInfoMetaMapperObj.userId,player_id.toLong))
+        log(sessionsByPlayer.toString)
+      bind("top",xhtml,
+          "user" -> Text("todo"),
+          "input" ->Text(
+          "[" +
+            sessionsByPlayer.map(session =>
+            "{ \"x\" : " + session.startTimeTranslation + "," +
+            "\"y\" : 40 },"
+            )
+
+            +"]"
 
 
-    )
 
 
+          ),
+          "graph" ->Text("todo")
+
+
+      )
+      }
+      case _ => log("Error, something went wrong.")
+        S.redirectTo("/index")
+
+    }
   }
+  //example format:
+  /*
+[ { "x" : 1,
+    "y" : 5
+  },
+
+]
+
+
+
+
+   */
 
 }
