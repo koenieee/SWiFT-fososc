b52ce1812ca68caaee0b80cfc86f57c12d3a7f37
koen-github <koenvdkruk@gmail.com>
2014-07-02 22:26:11 +0200

    Added Line chart, still some bugs

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/FluencyTimeSeries.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/FluencyTimeSeries.scala
@@ -5,7 +5,7 @@ import _root_.scala.xml._
 import net.liftweb.util.Helpers._
 import net.liftweb.http.S
 import net.liftweb.common.Full
-import org.ocbkc.swift.model.{SessionInfoMetaMapperObj, Player}
+import org.ocbkc.swift.model.{SessionInfo, SessionInfoMetaMapperObj, Player}
 import net.liftweb.mapper.By
 import org.ocbkc.swift.global.Logging._
 /**
@@ -14,49 +14,29 @@ import org.ocbkc.swift.global.Logging._
 class FluencyTimeSeries {
 
   def render(xhtml:NodeSeq):NodeSeq=
-  {
-    S.param("player") match {
-      case Full(player_id) =>
-      {
-       // val player = Player.find(By(Player.id, player_id.toLong))
-        val sessionsByPlayer = SessionInfoMetaMapperObj.find(By(SessionInfoMetaMapperObj.userId,player_id.toLong))
+  { S.param("player") match
+    { case Full(player_id) =>
+      { val sessionsByPlayer:List[SessionInfo] = SessionInfoMetaMapperObj.findAll(By(SessionInfoMetaMapperObj.userId,player_id.toLong))
         log(sessionsByPlayer.toString)
-      bind("top",xhtml,
-          "user" -> Text("todo"),
-          "input" ->Text(
-          "[" +
-            sessionsByPlayer.map(session =>
-            "{ \"x\" : " + session.startTimeTranslation + "," +
-            "\"y\" : 40 },"
-            )
+       
+        bind("top",xhtml,
+            "user"    -> Text("todo"),
+            "input"   ->Text(
+            "[" +
+              sessionsByPlayer.map(session =>
+              "{ \"time\" : " + session.startTimeTranslation + ",\n" +
+              "\"fluency\" : 40 },"
+              ).mkString.dropRight(1)
 
-            +"]"
-
-
-
-
-          ),
-          "graph" ->Text("todo")
-
-
+              +"]"
+            ),
+            "graph"   ->Text("todo")
       )
       }
-      case _ => log("Error, something went wrong.")
+      case _ =>
+      { log("Error, something went wrong.")
         S.redirectTo("/index")
-
+      }
     }
   }
-  //example format:
-  /*
-[ { "x" : 1,
-    "y" : 5
-  },
-
-]
-
-
-
-
-   */
-
 }
