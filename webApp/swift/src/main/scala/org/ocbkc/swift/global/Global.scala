49aeec05e3da505b12a4519c8acb8d19b1b0132d
Chide <cg@xs4all.nl>
2014-07-08 21:05:55 +0200

    minor

--- webApp/swift/src/main/scala/org/ocbkc/swift/global/Global.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/global/Global.scala
@@ -149,7 +149,7 @@ object TestSettings
    }
 
    val AUTOTRANSLATION                 = false // true
-   val CREATETESTUSERBASE              = true // false
+   val CREATETESTUSERBASE              = false
    /* <&y2012.09.29.19:44:55& TODO: if constitutions DO exist, don't create new constitutions. Or perhaps better: erase them but not before prompting the developer> */
    val CREATEDUMMYCONSTITUTIONS        = false // true // creates a number of constitutions with several updates and releases, but also some users.
    val STARTJARASIMULATIONDURINGBOOT   = false // Simulate playing with Jara during Boot. After boot normal playing (by real persons) can be continued from there.
