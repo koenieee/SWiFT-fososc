67954ac26567e8cb186a1fec3198779c54f35053
Jashafa <jashafa@kamping.zottekop.nl>
2014-10-14 22:14:04 +0200

    develop.ocevohut in progress

--- webApp/swift/src/main/scala/org/ocbkc/swift/OCBKC/Ocevohut.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/OCBKC/Ocevohut.scala
@@ -3,6 +3,8 @@ package org.ocbkc.swift
 import scala.util.Random
 import org.ocbkc.generic._
 import org.ocbkc.generic.random._
+import scala.math._
+
 
 package object ocevohut
 {  val ranSeq = new Random
@@ -10,13 +12,33 @@ package object ocevohut
 
 package ocevohut
 {
-import scala.math._
 
 object Types
-{  type SelectiveFitnessFunctionType[Genotype__TP] = Genotype__TP => Double
+{  type FitnessFunctionType[Genotype__TP] = Genotype__TP => Double
 }
 
 import Types._
+
+/** 
+  * For testing purposes
+  */
+class Main
+{  def main(args:Array[String])
+   {  object Types
+      {  type _3DpointGenotype = (Int, Int, Int)
+      }
+
+      import Types._
+
+      class _3Dpoint extends Individual[_3DpointGenotype]
+
+      /** Fitness is the distance to the optimal point at (4,5,98)
+        */
+      def oFiFun(_3dpg:_3DpointGenotype):Double =
+      {  sqrt( pow( 4d - _3dpg._1, 2d ) + pow( 5d  - _3dpg._2, 2d ) + pow(98d - _3dpg._3, 2d) )
+      }
+   }
+}
 
 /** @param populationSize size of the population in iterations > 0
   * @param oFiFun objective fitness function
@@ -31,8 +53,7 @@ trait OcevohutTrait[Genotype__TP]
    val numberOfClones:Int
    val numberOfChildren = populationSize - numberOfClones
    
-
-   val oFiFun = null // TODO
+   def oFiFun(g:Genotype__TP):Double = 0d // TODO
 
    /** @returns a Map with items (Individual__TP, number_of_children assigned to this parent)
      */
@@ -41,7 +62,7 @@ trait OcevohutTrait[Genotype__TP]
       null
    }
 
-   def selectClones:List[Individual__TP] =
+   def selectClones:List[IndividualType] =
    {  // TODO
       null
    }
@@ -51,27 +72,30 @@ trait CreateScaledFitnessFunctionTrait[Genotype__TP]
 {  def apply(pop:List[Individual[Genotype__TP]]):LocalSelectiveFitnessFunction[Genotype__TP]
 }
 
-class LocalSelectiveFitnessFunction[Genotype__TP]
+trait LocalSelectiveFitnessFunction[Genotype__TP]
 {  val map:Map[Genotype__TP, Double]
-   def apply(gt:Genotype__TP):Option[Double]
-   {  map(gt)
+   def apply(gt:Genotype__TP):Option[Double] =
+   {  map.get(gt)
    }
 }
 
-class CreateSigmaScaledFitnessFunction{Genotype__TP] extends CreateScaledFitnessFunctionTrait[Genotype__TP]
+class CreateSigmaScaledFitnessFunction[Genotype__TP] extends CreateScaledFitnessFunctionTrait[Genotype__TP]
 {  override def apply(pop:List[Individual[Genotype__TP]]):LocalSelectiveFitnessFunction[Genotype__TP] =
    {  null // TODO
    }
 }
 
+
+/** @todo perhaps more elegant to also allow global selective fitness functions, for that redesign of LocalSelectiveFitnessFunction is needed (it should have the same type as a global one) - perhaps using (possibly) partially defined functions.
+  */
 trait ProportionalSelectionTrait[Genotype__TP]
-{  def apply(pop:List[Individual[Genotype__TP]], selFiFun:SelectiveFitnessFunctionType[Genotype__TP]):Map[Individual[Genotype__TP], Int]
+{  def apply(pop:List[Individual[Genotype__TP]], selFiFun:LocalSelectiveFitnessFunction[Genotype__TP]):Map[Individual[Genotype__TP], Int]
 }
 
 class SUS[Genotype__TP] extends ProportionalSelectionTrait[Genotype__TP]
-{  override def apply(pop:List[Individual[Genotype__TP]], selFiFun:SelectiveFitnessFunctionType[Genotype__TP]):Map[Individual[Genotype__TP], Int] =
+{  override def apply(pop:List[Individual[Genotype__TP]], selFiFun:LocalSelectiveFitnessFunction[Genotype__TP]):Map[Individual[Genotype__TP], Int] =
    {  val n:Int = pop.size
-      val totalFitness:Double    = pop.map{ i => selFiFun(i.genotype) }.foldLeft(0d)(_+_)
+      val totalFitness:Double    = pop.map{ i => selFiFun(i.genotype).get }.fold(0d){(a:Double,b:Double)=>a+b}
       val averageFitness:Double  = totalFitness/n
       val gridLength:Double      = averageFitness // just a synonym, that is same extension, however, different intension (Rudolf Carnap, the morning star is the evening star" - ;-) )
       val randomShift:Double     = RandomExtras.nextBetween(ranSeq, 0d, averageFitness)
