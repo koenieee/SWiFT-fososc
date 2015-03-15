937734f62d5bf817637caee50a7b4427d3401bc7
Koen van der Kruk <koenvdkruk@gmail.com>
2015-03-15 16:17:56 +0100

    Added sigma scaling pop by Koen

--- webApp/swift/src/test/scala/scalaTest/SigmaScalingTesting.scala
+++ webApp/swift/src/test/scala/scalaTest/SigmaScalingTesting.scala
@@ -11,39 +11,60 @@ import scala.math._
 class SigmaScalingTesting extends FlatSpec with GivenWhenThen
 { "Sigma Scaling" must " be correctly runned" in
   {
-
-     object Types
-     {  type _3DpointGenotype = (Int, Int, Int)
+     class TestStrings(inword: String) extends Individual[String]
+     {  if(inword.toList.count(_.isInstanceOf[Char]) != 11)
+        {  throw new IllegalStateException("This word: " + inword + " has not the length of 11")
+        }
+        val genotype = inword
+        override def toString: String = "[String = " + inword + " ]"
      }
 
-     import Types._
-
-     // The individuals of the population are 3D points in space
-     class _3Dpoint(x:Int, y:Int, z:Int) extends Individual[_3DpointGenotype]
-     {  val genotype = (x,y,z)
-        override def toString: String = "[3DPoint x="+x+" y="+y+" z="+z+"]" //added this one, to see in this testing case also the output of 3D point (instead of memory addresses)
+     def getIntByChar(cs: Char): Int =
+     {  val listAlfa:List[(Char, Int)]  = "abcdefghijklmnopqrstuvwxyz ".toUpperCase.toList.zipWithIndex
+        listAlfa.find(e => e._1 == cs).get._2+1
      }
 
-     def oFiFun(_3dpg:_3DpointGenotype):Double =
-     {  1 / (sqrt( pow( 4d - _3dpg._1, 2d ) + pow( 5d  - _3dpg._2, 2d ) + pow(98d - _3dpg._3, 2d) ) + 0.0001)
+     def genIntList(exL: List[Char] = List()): List[Int] =
+     {  exL match {
+           case Nil => Nil
+           case head::tail => getIntByChar(head) :: genIntList(tail)
+        }
      }
 
-     val Sigmafor3dpg = new CreateSigmaScaledFitnessFunction[_3DpointGenotype]
+     /** This function returns a percentage between 0 .. 1 of how much a input word comes close to the target word (Hello World)
+       *  So if the word: Hello World is in alphabetical number: List(8, 5, 12, 12, 15, 27, 23, 15, 18, 12, 4),
+       *  and another word: Alloa Earth is:  List(1, 12, 12, 15, 1, 27, 5, 1, 18, 20, 8)
+       *  the percentage that Alloa Earth matches Hello World is 0.2727272727272727, so about 27 percent.
+
+      */
+     def oFiFun(word: String):Double =
+     {  val intList = genIntList(word.toUpperCase.toList)
+        val target: List[Int] = List(8, 5, 12, 12, 15, 27, 23, 15, 18, 12, 4) //"the perfect fitness " or target => text: Hello World in uppercase
+        val zippedList = intList zip target
+        val compare = zippedList.map(item => item._1 == item._2)
+        val countGoodOnes = compare count(_ == true)
+        (countGoodOnes * 100d / 11d ) / 100d //11 comes from 11 elements of 'the one', see perfect above
+     }
+
+     val SigmaforString = new CreateSigmaScaledFitnessFunction[String]
 
      val test_pop = List(
-        new _3Dpoint(1,2,3),
-        new _3Dpoint(3,5,90),
-        new _3Dpoint(6,5,100),
-        new _3Dpoint(4,5,97)
+        new TestStrings("Hallo Werel"),
+        new TestStrings("Allo Walrod"),
+        new TestStrings("Kiplo Stakl"),
+        new TestStrings("Hello World"),
+        new TestStrings("VisseEnzovr")
      )
+     info("Test Populuation: " + test_pop)
 
-     val globalSelFunc:FitnessFunctionType[_3DpointGenotype] = new FitnessFunctionType[_3DpointGenotype]
-     {  override def apply(gt:_3DpointGenotype):Double =
-     {  oFiFun(gt)
-     }
+     val globalSelFunc:FitnessFunctionType[String] = new FitnessFunctionType[String]
+     {  override def apply(gt:String):Double =
+        {  oFiFun(gt)
+        }
      }
 
-     info("Final output: " + Sigmafor3dpg(test_pop, globalSelFunc))
+     info("Final output: " + SigmaforString(test_pop, globalSelFunc).apply("Hallo World")) //output:
+
   }
 }
 
