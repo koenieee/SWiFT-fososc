c5a980b10452ebc7edab99836a2b1dd208b0eab2
Koen van der Kruk <koenvdkruk@gmail.com>
2015-07-05 15:42:08 +0200

    Updated Constitutions using new binding way, not working atm

--- webApp/swift/src/main/scala/org/ocbkc/swift/snippet/Constitutions.scala
+++ webApp/swift/src/main/scala/org/ocbkc/swift/snippet/Constitutions.scala
@@ -4,7 +4,7 @@ package snippet
 {
 import _root_.scala.xml._
 import _root_.net.liftweb.util._
-import _root_.net.liftweb.http._
+import net.liftweb.http._
 import _root_.net.liftweb.common._
 import _root_.java.util.Date
 import org.ocbkc.swift.lib._
@@ -18,6 +18,7 @@ import org.ocbkc.swift.model._
 import org.ocbkc.swift.general.GUIdisplayHelpers._
 import org.ocbkc.swift.global._
 import org.ocbkc.swift.test._
+import org.ocbkc.swift.global.Logging._
 
 class Constitutions
 {  val sesCoordLR = SesCoord.is; // extract session coordinator object from session variable.
@@ -28,8 +29,8 @@ class Constitutions
 
    val options = TableSorter.options(headers,sortList)
 */
-   def list(ns: NodeSeq): NodeSeq =
-   {  def displayConstis:NodeSeq = 
+   def list =
+   {  def displayConstis: List[CssSel] =
       {  /*
          if(SesCoord.Test.initConstitutions) // only for testing, remove after test.
          {  val c1 = Constitution.create(0) // test
@@ -39,32 +40,22 @@ class Constitutions
             SesCoord.Test.initConstitutions = false
          }
          */
-         if( Constitution.constis.size == 0 )
-            Text("There is no constitution population yet...")
-         else
-         {  // val doc =  """"<ul>""" + Constitution.constis.map(c => """  <li> Constitution """ + c.constiId + "</li>").foldLeft("")((a,b) => a + "\n" + b) + "\n</ul>"
+          // val doc =  """"<ul>""" + Constitution.constis.map(c => """  <li> Constitution """ + c.constiId + "</li>").foldLeft("")((a,b) => a + "\n" + b) + "\n</ul>"
             // val doc =  <ul> Constitution.constis.map(c => <li> Constitution { c.constiId } </li>).foldLeft("")((a,b) => a b) </ul>
             implicit val displayIfNone = "-"
             val df = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm")
-            val doc = 
-            Elem(
-               null,
-               "table",
-               new UnprefixedAttribute("id", Text("constitutionsTable"), new UnprefixedAttribute("class", Text("tablesorter"), Null)),
-               TopScope,
-               false,
-               <thead><tr><th>id</th><th>description</th><th>fluency</th><th>APC</th><th>ADT (ms)</th><th>Creation date</th></tr></thead>,
-               <tbody>{ Constitution.constis.sortWith((c1,c2) => c1.constiId > c2.constiId ).map(
-                           c => <tr><td><a href={ "constitution?id=" + c.constiId  }>{ c.constiId }</a></td><td>{ c.shortDescription }</td><td>{ optionToUI(ConstiScores.averageFluencyLatestReleaseWithScore(c.constiId).collect{ case afs:(VersionId,Double) => afs._2 } ) }</td><td>{ optionToUI(ConstiScores.averagePercentageCorrect(GlobalConstant.AveragePercentageCorrect.minimalNumberOfSessionsPerPlayer, c.constiId)) }</td><td>{ optionToUI(ConstiScores.averageDurationTranslation(GlobalConstant.AverageDurationTranslation.minimalNumberOfSessionsPerPlayer, c.constiId)) }</td><td>{ df.format(c.creationTime).toString }</td></tr>)
-               }
-               </tbody>
-            )
-            // <&y2012.05.28.12:13:54& perhaps more elegant to refer to constitutions by using a html-parameter>
-            // <&y2012.06.29.22:54:28& COULDDO optimise sorting function, by doing it only once, it is now done everytime.>
-            println("   doc = " + doc)
-            //XML.loadString(doc)
-            doc
-         }
+
+            Constitution.constis.sortWith((c1,c2) => c1.constiId > c2.constiId ).map{
+            c =>
+               ".id *" #> { <a href={ "constitution?id=" + c.constiId  }>{ c.constiId }</a> } &
+               ".description *" #> { c.shortDescription } &
+               ".fluency *"   #>  { optionToUI(ConstiScores.averageFluencyLatestReleaseWithScore(c.constiId).collect{ case afs:(VersionId,Double) => afs._2 } ) } &
+               ".APC *"       #> { optionToUI(ConstiScores.averagePercentageCorrect(GlobalConstant.AveragePercentageCorrect.minimalNumberOfSessionsPerPlayer, c.constiId)) } &
+               ".ADT *"       #> { optionToUI(ConstiScores.averageDurationTranslation(GlobalConstant.AverageDurationTranslation.minimalNumberOfSessionsPerPlayer, c.constiId)) } &
+               ".creationDate *"   #>  { df.format(c.creationTime) }
+               //
+            }
+
       }
       
       def processCreateNewBt() =
@@ -79,11 +70,30 @@ class Constitutions
          S.redirectTo("constitution.html?id=" + const.constiId + "&edit=true&firstedit=true") // <&y2012.06.05.10:05:58& redirectTo, does this also terminate the execution of the method, or does the method remain indefinitely on the stack?>
       }
 
-      val answer   = bind( "top", ns, 
-                           "constisInTable"  -> displayConstis,
-                           "createNewBt"    -> SHtml.button("Create", processCreateNewBt) // <&y2012.05.25.10:09:21& disable button when no user is logged in>
-                         )
-      answer
+      if( Constitution.constis.size == 0 ){
+         log("No constitutions found")
+         "#constitutionsTable" #> Text("There is no constitution population yet...")
+      }
+       else{
+         log("Generating constiTable")
+         ".tableRows *" #> {
+            implicit val displayIfNone = "-"
+            val df = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm")
+
+            Constitution.constis.sortWith((c1,c2) => c1.constiId > c2.constiId ).map{
+               c =>
+                  log("ConstiID: " + c.constiId)
+                     ".id *" #> SHtml.link("constitution?id=" + c.constiId, () => (), Text(c.constiId.toString))
+                     ".description *" #>  c.shortDescription
+                     ".fluency *"   #>   optionToUI(ConstiScores.averageFluencyLatestReleaseWithScore(c.constiId).collect{ case afs:(VersionId,Double) => afs._2 } )
+                     ".APC *"       #>  optionToUI(ConstiScores.averagePercentageCorrect(GlobalConstant.AveragePercentageCorrect.minimalNumberOfSessionsPerPlayer, c.constiId))
+                     ".ADT *"       #>  optionToUI(ConstiScores.averageDurationTranslation(GlobalConstant.AverageDurationTranslation.minimalNumberOfSessionsPerPlayer, c.constiId))
+                     ".creationDate *"   #>   df.format(c.creationTime)
+               //
+            }
+         }
+      }
+      "#createNewBt"    #> SHtml.button("Create", processCreateNewBt) // <&y2012.05.25.10:09:21& disable button when no user is logged in>
    }
 }
 
